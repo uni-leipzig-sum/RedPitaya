647ae21f314ac1ac8e917855ff71e8b786080d8c
Lukas Botsch <lukas.botsch@uni-leipzig.de>
2020-06-20 10:46:34 -0400

    Fixed fpga counter testbench

--- fpga/prj/counter/tbn/red_pitaya_counter_tb.sv
+++ fpga/prj/counter/tbn/red_pitaya_counter_tb.sv
@@ -21,7 +21,7 @@
 
 module red_pitaya_counter_tb #(
    // time periods
-   realtime     TP = 8.0ns, // 125MHz
+   realtime     TP = 8.0ns // 125MHz
 );
 
 
@@ -30,21 +30,21 @@ module red_pitaya_counter_tb #(
 ////////////////////////////////////////////////////////////////////////////////
 
    function count_signal_a (input int unsigned cyc);
-      // Toggle a counting signal every 100 clock cycles (== 800ns)
-      count_signal_a = (cyc % 100 == 0) ? 1'b1 : 1'b0;
+      // Toggle a counting signal every 10 clock cycles (== 80ns)
+      count_signal_a = (cyc % 10 == 0) ? 1'b1 : 1'b0;
    endfunction: count_signal_a
 
    function count_signal_b (input int unsigned cyc);
-      // Send a counting signal every 50 clock cycles (== 400ns) and hold it high for 10 cycles
-      count_signal_b = (cyc % 50 < 10) ? 1'b1 : 1'b0;
+      // Send a counting signal every 5 clock cycles (== 40ns) and hold it high for 2 cycles
+      count_signal_b = (cyc % 5 < 2) ? 1'b1 : 1'b0;
    endfunction: count_signal_b
 
-   logic        clk;
-   logic        rstn;
-   logic        signal [4-1:0];
+   logic          clk;
+   logic          rstn;
+   logic [4-1:0]  signal;
 
-   assign signal[0] = count_signal_a;
-   assign signal[1] = count_signal_b;
+   assign signal[0] = count_signal_a(clk_cyc);
+   assign signal[1] = count_signal_b(clk_cyc);
 
    // Clock signal
    initial clk = 1'b0;
@@ -57,6 +57,11 @@ module red_pitaya_counter_tb #(
       rstn = 1'b1;
    end
 
+   initial begin
+      signal[2] = 0;
+      signal[3] = 0;
+   end
+
    // Clock cycle
    int unsigned clk_cyc = 0;
    always_ff @(posedge clk)
@@ -66,44 +71,86 @@ module red_pitaya_counter_tb #(
 // test sequence
 ////////////////////////////////////////////////////////////////////////////////
 
-   logic [ 32-1: 0] sys_addr ;
-   logic [ 32-1: 0] sys_wdata;
-   logic            sys_wen  ;
-   logic            sys_ren  ;
+   logic [ 32-1: 0] sys_addr  = 0;
+   logic [ 32-1: 0] sys_wdata = 0;
+   logic            sys_wen   = 0;
+   logic            sys_ren   = 0;
    logic [ 32-1: 0] sys_rdata;
-   logic            sys_err  ;
-   logic            sys_ack  ;
+   logic            sys_err;
+   logic            sys_ack;
 
-   logic [32-1: 0]  counts_1;
-   logic [32-1: 0]  counts_2;
-   logic [32-1: 0]  rdata;
+   logic [32-1: 0]  counts_1  = 0;
+   logic [32-1: 0]  counts_2  = 0;
+   logic [32-1: 0]  rdata     = 0;
 
-   logic            failure;
+   logic            failure   = 0;
+
+// --- SYSTEM BUS TASKS ---
+
+task bus_transaction (
+  input  logic we,
+  input  [32-1:0] addr,
+  input  [32-1:0] wdata,
+  output [32-1:0] rdata
+);
+  #1;
+  sys_wen    <=  we;
+  sys_ren    <= ~we;
+  sys_addr   <= addr;
+  sys_wdata  <= wdata;
+  #1;
+  sys_wen    <= 1'b0;
+  sys_ren    <= 1'b0;
+  while (~sys_ack & ~sys_err)
+  #1;
+  rdata <= sys_rdata;
+  #1;
+endtask: bus_transaction
+
+// bus write transfer
+task bus_write (
+  input  [32-1:0] addr,
+  input  [32-1:0] wdata
+);
+  logic [32-1:0] rdata;
+  bus_transaction (.we (1'b1), .addr (addr), .wdata (wdata), .rdata (rdata));
+endtask: bus_write
+
+// bus read transfer
+task bus_read (
+  input  [32-1:0] addr,
+  output [32-1:0] rdata
+);
+  bus_transaction (.we (1'b0), .addr (addr), .wdata ('x), .rdata (rdata));
+endtask: bus_read
+
 
    initial begin
+      $display ("-- Waiting for reset sequence");
       // Wait for initialization to finish
       wait (rstn)
         repeat(10) @(posedge clk);
 
+      $display ("-- Configuring counter");
       // Configure counter
-      bus.write(32'h04, 32'd1000);    // set timeout to 1000 clk cycles (=8us)
-      bus.write(32'h10, 32'h1000);    // use all bins (4096)
-      bus.write(32'h14, 32'h0);       // no repetitions per bin
-      bus.write(32'h18, 32'h0);       // no predelay
-      bus.write(32'h1C, 32'h8004);    // trigger when signal[3] gets high, no gating
+      bus_write(32'h04, 32'd100);    // set timeout to 100 clk cycles (=8us)
+      bus_write(32'h10, 32'h1000);    // use all bins (4096)
+      bus_write(32'h14, 32'h0);       // no repetitions per bin
+      bus_write(32'h18, 32'h0);       // no predelay
+      bus_write(32'h1C, 32'h8004);    // trigger when signal[3] gets high, no gating
 
       // Count immediately
       failure = 0;
       $display ("-- countImmediately");
-      bus.write(32'h00, 32'h03);      // send command countImmediately
+      bus_write(32'h00, 32'h03);      // send command countImmediately
       repeat(10) @(posedge clk);
-      bus.read(32'h00, rdata);           // read counter state
+      bus_read(32'h00, rdata);           // read counter state
       $display ("counter state (expected 2): $d", rdata);
       failure = (failure || (rdata != 2));
-      repeat(1010) @(posedge clk);       // Allow for counting to end
-      bus.read(32'h08, counts_1);        // read counter ch1
-      bus.read(32'h1C, counts_2);        // read counter ch2
-      bus.read(32'h00, rdata);           // read counter state
+      repeat(110) @(posedge clk);        // Allow for counting to end
+      bus_read(32'h08, counts_1);        // read counter ch1
+      bus_read(32'h1C, counts_2);        // read counter ch2
+      bus_read(32'h00, rdata);           // read counter state
       $display ("counter state (expected 0): $d", rdata);
       failure = (failure || (rdata != 0));
       $display ("counter ch1 (expected 100): %d", counts_1);
@@ -116,32 +163,32 @@ module red_pitaya_counter_tb #(
       // Count triggered, using software trigger command
       failure = 0;
       $display ("-- countTriggered (sw trigger)");
-      bus.write(32'h00, 32'h02);         // send command reset
+      bus_write(32'h00, 32'h02);         // send command reset
       repeat(10) @(posedge clk);
-      bus.read(32'h20, rdata);           // read bin index
+      bus_read(32'h20, rdata);           // read bin index
       $display ("bin index (expected 0):     $d", rdata);
       failure = (failure || (rdata != 0));
-      bus.read(32'h00, rdata);           // read counter state
+      bus_read(32'h00, rdata);           // read counter state
       $display ("counter state (expected 0): $d", rdata);
       failure = (failure || (rdata != 0));
-      bus.write(32'h00, 32'h04);         // send command countTriggered
+      bus_write(32'h00, 32'h04);         // send command countTriggered
       repeat(10) @(posedge clk);
-      bus.read(32'h00, rdata);           // read counter state
+      bus_read(32'h00, rdata);           // read counter state
       $display ("counter state (expected 3): $d", rdata);
       failure = (failure || (rdata != 3));
-      bus.write(32'h00, 32'h06);         // send command trigger
+      bus_write(32'h00, 32'h06);         // send command trigger
       repeat(10) @(posedge clk);
-      bus.read(32'h00, rdata);           // read counter state
+      bus_read(32'h00, rdata);           // read counter state
       $display ("counter state (expected 7): $d", rdata);
       failure = (failure || (rdata != 7));
-      repeat(1010) @(posedge clk);       // Allow for counting to end
+      repeat(110) @(posedge clk);        // Allow for counting to end
       $display ("bin index (expected 1):     $d", rdata);
       failure = (failure || (rdata != 1));
-      bus.read(32'h00, rdata);           // read counter state
+      bus_read(32'h00, rdata);           // read counter state
       $display ("counter state (expected 3): $d", rdata);
       failure = (failure || (rdata != 3));
-      bus.read(32'h10000, counts_1);     // read counter 1, bin 1
-      bus.read(32'h14000, counts_2);     // read counter 2, bin 1
+      bus_read(32'h10000, counts_1);     // read counter 1, bin 1
+      bus_read(32'h14000, counts_2);     // read counter 2, bin 1
       $display ("counter ch1 (expected 100): %d", counts_1);
       failure = (failure || (counts_1 != 100));
       $display ("counter ch2 (expected 200): %d", counts_2);
@@ -152,34 +199,34 @@ module red_pitaya_counter_tb #(
       // Count triggered, using external trigger signal
       failure = 0;
       $display ("-- countTriggered (ext trigger)");
-      bus.write(32'h00, 32'h02);         // send command reset
+      bus_write(32'h00, 32'h02);         // send command reset
       repeat(10) @(posedge clk);
-      bus.read(32'h20, rdata);           // read bin index
+      bus_read(32'h20, rdata);           // read bin index
       $display ("bin index (expected 0):     $d", rdata);
       failure = (failure || (rdata != 0));
-      bus.read(32'h00, rdata);           // read counter state
+      bus_read(32'h00, rdata);           // read counter state
       $display ("counter state (expected 0): $d", rdata);
       failure = (failure || (rdata != 0));
-      bus.write(32'h00, 32'h04);         // send command countTriggered
+      bus_write(32'h00, 32'h04);         // send command countTriggered
       repeat(10) @(posedge clk);
-      bus.read(32'h00, rdata);           // read counter state
+      bus_read(32'h00, rdata);           // read counter state
       $display ("counter state (expected 3): $d", rdata);
       failure = (failure || (rdata != 3));
       signal[4-1] = 1'b1;                // set input 4 high -- trigger signal
       repeat(10) @(posedge clk);
       signal[4-1] = 1'b0;                // set input 4 low
       repeat(10) @(posedge clk);
-      bus.read(32'h00, rdata);           // read counter state
+      bus_read(32'h00, rdata);           // read counter state
       $display ("counter state (expected 7): $d", rdata);
       failure = (failure || (rdata != 7));
-      repeat(1010) @(posedge clk);       // Allow for counting to end
+      repeat(110) @(posedge clk);       // Allow for counting to end
       $display ("bin index (expected 1):     $d", rdata);
       failure = (failure || (rdata != 1));
-      bus.read(32'h00, rdata);           // read counter state
+      bus_read(32'h00, rdata);           // read counter state
       $display ("counter state (expected 3): $d", rdata);
       failure = (failure || (rdata != 3));
-      bus.read(32'h10000, counts_1);     // read counter 1, bin 1
-      bus.read(32'h14000, counts_2);     // read counter 2, bin 1
+      bus_read(32'h10000, counts_1);     // read counter 1, bin 1
+      bus_read(32'h14000, counts_2);     // read counter 2, bin 1
       $display ("counter ch1 (expected 100): %d", counts_1);
       failure = (failure || (counts_1 != 100));
       $display ("counter ch2 (expected 200): %d", counts_2);
@@ -190,31 +237,31 @@ module red_pitaya_counter_tb #(
       // Count gated
       failure = 0;
       $display ("-- countGated (ext trigger)");
-      bus.write(32'h00, 32'h02);         // send command reset
+      bus_write(32'h00, 32'h02);         // send command reset
       repeat(10) @(posedge clk);
-      bus.read(32'h20, rdata);           // read bin index
+      bus_read(32'h20, rdata);           // read bin index
       $display ("bin index (expected 0):     $d", rdata);
       failure = (failure || (rdata != 0));
-      bus.read(32'h00, rdata);           // read counter state
+      bus_read(32'h00, rdata);           // read counter state
       $display ("counter state (expected 0): $d", rdata);
       failure = (failure || (rdata != 0));
-      bus.write(32'h00, 32'h05);         // send command countGated
+      bus_write(32'h00, 32'h05);         // send command countGated
       repeat(10) @(posedge clk);
-      bus.read(32'h00, rdata);           // read counter state
+      bus_read(32'h00, rdata);           // read counter state
       $display ("counter state (expected 8): $d", rdata);
       failure = (failure || (rdata != 8));
       signal[4-1] = 1'b1;                // set input 4 high -- gate signal
-      repeat(1000) @(posedge clk);       // keep gate high for 1000 clock cycles
+      repeat(100) @(posedge clk);        // keep gate high for 100 clock cycles
       signal[4-1] = 1'b0;                // set input 4 low
       repeat(10) @(posedge clk);         // allow for count storage
-      bus.read(32'h00, rdata);           // read counter state
+      bus_read(32'h00, rdata);           // read counter state
       $display ("bin index (expected 1):     $d", rdata);
       failure = (failure || (rdata != 1));
-      bus.read(32'h00, rdata);           // read counter state
+      bus_read(32'h00, rdata);           // read counter state
       $display ("counter state (expected 8): $d", rdata);
       failure = (failure || (rdata != 8));
-      bus.read(32'h10000, counts_1);     // read counter 1, bin 1
-      bus.read(32'h14000, counts_2);     // read counter 2, bin 1
+      bus_read(32'h10000, counts_1);     // read counter 1, bin 1
+      bus_read(32'h14000, counts_2);     // read counter 2, bin 1
       $display ("counter ch1 (expected 100): %d", counts_1);
       failure = (failure || (counts_1 != 100));
       $display ("counter ch2 (expected 200): %d", counts_2);
@@ -222,25 +269,14 @@ module red_pitaya_counter_tb #(
       if (failure) $display ("FAILURE");
       else $display ("SUCCESS");
 
+      repeat (10) @(posedge clk);
+      $finish ();
    end
 
 ////////////////////////////////////////////////////////////////////////////////
 // module instances
 ////////////////////////////////////////////////////////////////////////////////
 
-sys_bus_model bus (
-  // system signals
-  .clk          (clk      ),
-  .rstn         (rstn     ),
-  // bus protocol signals
-  .sys_addr     (sys_addr ),
-  .sys_wdata    (sys_wdata),
-  .sys_wen      (sys_wen  ),
-  .sys_ren      (sys_ren  ),
-  .sys_rdata    (sys_rdata),
-  .sys_err      (sys_err  ),
-  .sys_ack      (sys_ack  ) 
-);
 
 red_pitaya_counter i_counter (
       // signals
@@ -261,9 +297,10 @@ red_pitaya_counter i_counter (
 // waveforms
 ////////////////////////////////////////////////////////////////////////////////
 
+/*
 initial begin
   $dumpfile("red_pitaya_counter_tb.vcd");
   $dumpvars(0, red_pitaya_counter_tb);
 end
-
+*/
 endmodule: red_pitaya_counter_tb
